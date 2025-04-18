b5b12cb472f3e25462e9ec050472c0fcf1772982
ahmedjamal752 <jamaleddineahmed972@gmail.com>
2025-04-18 20:40:37 +0100

    fonction getclientinfo

--- src/php/connectDB.php
+++ src/php/connectDB.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost", "root", "", "clubsportif", 3306);
+$conn = new mysqli("localhost", "root", "admin", "clubsportif", 3306);
 
 if ($conn->connect_error) {
     die("erreur de connection" . $conn->connect_error);
--- src/php/functions/getclientinfo.php
+++ src/php/functions/getclientinfo.php
@@ -1,26 +1,22 @@
-<?php 
-require_once 'connectDB.php';
-function getClientByEmail($email) {
-    global $conn;
+<?php
 
+function getClientByEmail(mysqli $mysqli, string $email)
+{
     // Préparation de la requête
-    $stmt = $conn->prepare("SELECT clientId, nom, prenom, dateNais, email, tel, password FROM client WHERE email = ?");
-    
+    $stmt = $mysqli->prepare("SELECT clientId, nom, prenom, dateNais, email, tel, password FROM client WHERE email = ?");
+
     if ($stmt) {
         $stmt->bind_param("s", $email); // "s" = string
         $stmt->execute();
         $result = $stmt->get_result();
 
-        // Retourner le client ou false si non trouvé
-        return $result->fetch_assoc();
+        echo "hello";
+        print_r($result->fetch_assoc());
     } else {
-        // En cas d'erreur de préparation
-        return false;
         echo "erreur de preparation de la requete";
+        return false;
     }
 }
-
-
 
 
 
